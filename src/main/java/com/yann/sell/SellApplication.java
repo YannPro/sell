66e58e0ef366cf8e2433e665cd457b523d98ffca
YannPro <849723885@qq.com>
2017-11-26 19:47:31 +0800

    2017-11-25 15:49

--- src/main/java/com/yann/sell/SellApplication.java
+++ src/main/java/com/yann/sell/SellApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class SellApplication extends SpringBootServletInitializer {
+public class SellApplication /*extends SpringBootServletInitializer implements WebApplicationInitializer */{
 
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SellApplication.class);
-	}
+/*	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringApplication.class);
+	}*/
 	public static void main(String[] args) {
 		SpringApplication.run(SellApplication.class, args);
 	}
